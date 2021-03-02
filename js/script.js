b49f3d67850bc0708e75a99c514045661ab3d44e
Daniele <danielelai86@gmail.com>
2021-03-02 16:54:43 +0100

    Aggiunta stampa a video

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
 
 for (var i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0 ) {
-    console.log("FizzBuzz");
+    document.getElementById('lista').innerHTML += "<li>" + ["FizzBuzz"] + "</li>"
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+    document.getElementById('lista').innerHTML += "<li>" + ["Fizz"] + "</li>"
   } else if (i % 5 == 0) {
-    console.log("Buzz");
+    document.getElementById('lista').innerHTML += "<li>" + ["Buzz"] + "</li>"
   } else {
-    console.log(i);
+    document.getElementById('lista').innerHTML += "<li>" + [i] + "</li>";
   }
 }
